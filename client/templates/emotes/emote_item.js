7f72143432765aaa57465268de6ed81874efe2ae
Sadiq Okocha <sokocha@gmail.com>
2015-06-24 15:43:37 +0100

    adds emoticons to emotes

---
+++ client/templates/emotes/emote_item.js
@@ -0,0 +1,23 @@
+Template.emoteItem.helpers({
+  emoticon: function () {
+    if (this.emotion === 'Jealous') {
+      return '😒';
+    } else if (this.emotion === 'Happy') {
+      return '😊';
+    } else if (this.emotion === 'Excited') {
+      return '😝';
+    } else if (this.emotion === 'Sad') {
+      return '😢';
+    } else if (this.emotion === 'Angry') {
+      return '😡';
+    } else if (this.emotion === 'Tender') {
+      return '😍';
+    } else if (this.emotion === 'Prideful') {
+      return '😏';
+    } else if (this.emotion === 'Meh') {
+      return '😕';
+    } else if (this.emotion === 'Ashamed') {
+      return '😅';
+    }
+  }
+});
