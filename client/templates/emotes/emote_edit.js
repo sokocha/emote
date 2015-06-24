5d02bcbff5e2d8f8275787276a5c4e0986ec1245
Sadiq Okocha <sokocha@gmail.com>
2015-06-24 16:44:26 +0100

    adds edit emote events

---
+++ client/templates/emotes/emote_edit.js
@@ -0,0 +1,26 @@
+Template.emoteEdit.events({ 
+
+  'submit form': function(e) {
+  e.preventDefault();
+  var currentEmoteId = this._id;
+  var emoteProperties = {
+    explanation: $(e.target).find('[name=explanation]').val()
+  }
+  Emotes.update(currentEmoteId, {$set: emoteProperties}, function(error) { if (error) {
+        // display the error to the user
+        alert(error.reason); } else {
+          Router.go('emotePage', {_id: currentEmoteId});
+        }
+      }); 
+    },
+
+  'click .delete': function(e) { 
+    e.preventDefault();
+
+    if (confirm("Delete this emote?"))  { 
+      var currentEmoteId = this._id; 
+      Emotes.remove(currentEmoteId); 
+      Router.go('emotesList');
+    } 
+  }
+});
