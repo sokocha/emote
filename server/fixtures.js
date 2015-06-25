fb3985b6504ae691eb0dfd37dc385652f94e28bb
Sadiq Okocha <sokocha@gmail.com>
2015-06-25 09:37:16 +0100

    adds comments collection

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,17 +1,55 @@
-// if (Emotes.find().count() === 0)  {
-//   Emotes.insert({
-//     explanation: "I wish I could be as cool as her",
-//     emotion: 'Jealous'
-//   });
+if (Emotes.find().count() === 0)  {
+  var now = new Date().getTime();
 
-//   Emotes.insert({
-//     explanation: " I get to travel to the moon!",
-//     emotion: 'Excited'
-//   });
+  // create two users
+  var tomId = Meteor.users.insert({ 
+    profile: { name: 'Tom Coleman' }
+  });
+  var tom = Meteor.users.findOne(tomId); 
 
-//   Emotes.insert({
-//     explanation: "Arsenal lost the league again!",
-//     emotion: 'Angry'
-//   });
-// }
+  var sachaId = Meteor.users.insert({
+      profile: { name: 'Sacha Greif' }
+    });
+
+  var sacha = Meteor.users.findOne(sachaId);
+
+  var jealousyId = Emotes.insert({
+    explanation: "I wish I could be as cool as her",
+    emotion: 'Jealous'
+  });
+
+  var excitementId = Emotes.insert({
+    explanation: " I get to travel to the moon!",
+    emotion: 'Excited'
+  });
+
+  var angerId = Emotes.insert({
+    explanation: "Arsenal lost the league again!",
+    emotion: 'Angry'
+  });
+
+  Comments.insert({
+    emoteId: excitementId,
+    userId: sacha._id,
+    author: sacha.profile.name,
+    submitted: new Date (now - 5 * 3500 * 1000),
+    body: "lol don't jealous me too much now. Keep trying and one day you'll get there."
+  });
+
+  Comments.insert({
+    emoteId: jealousyId,
+    userId: tom._id,
+    author: tom.profile.name,
+    submitted: new Date (now - 7 * 3500 * 1000),
+    body: "lol don't jealous me too much now. Keep trying and one day you'll get there."
+  });
+
+  Comments.insert({
+    emoteId: angerId,
+    userId: tom._id,
+    author: tom.profile.name,
+    submitted: new Date (now - 12 * 3500 * 1000),
+    body: "lol don't jealous me too much now. Keep trying and one day you'll get there."
+  });
+}
 
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,7 @@
 Meteor.publish('emotes', function()  {
   return Emotes.find();
-})
+});
+
+Meteor.publish('comments', function() {
+  return Comments.find();
+});
