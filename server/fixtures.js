fbc937e4b9b968c264dec21b69170a3c220c7a40
Sadiq Okocha <sokocha@gmail.com>
2015-06-25 11:26:51 +0100

    comments out fixtures data

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,58 +1,58 @@
-if (Emotes.find().count() === 0)  {
-  var now = new Date().getTime();
+// if (Emotes.find().count() === 0)  {
+//   var now = new Date().getTime();
 
-  // create two users
-  var tomId = Meteor.users.insert({ 
-    username: 'Tom Coleman'
-  });
-  var tom = Meteor.users.findOne(tomId); 
+//   // create two users
+//   var tomId = Meteor.users.insert({ 
+//     username: 'Tom Coleman'
+//   });
+//   var tom = Meteor.users.findOne(tomId); 
 
-  var sachaId = Meteor.users.insert({
-      username: 'Sacha Greif' 
-    });
+//   var sachaId = Meteor.users.insert({
+//       username: 'Sacha Greif' 
+//     });
 
-  var sacha = Meteor.users.findOne(sachaId);
+//   var sacha = Meteor.users.findOne(sachaId);
 
-  var jealousyId = Emotes.insert({
-    explanation: "I wish I could be as cool as her",
-    emotion: 'Jealous',
-    author: tom.username
-  });
+//   var jealousyId = Emotes.insert({
+//     explanation: "I wish I could be as cool as her",
+//     emotion: 'Jealous',
+//     author: tom.username
+//   });
 
-  var excitementId = Emotes.insert({
-    explanation: " I get to travel to the moon!",
-    emotion: 'Excited',
-    author: sacha.username
-  });
+//   var excitementId = Emotes.insert({
+//     explanation: " I get to travel to the moon!",
+//     emotion: 'Excited',
+//     author: sacha.username
+//   });
 
-  var angerId = Emotes.insert({
-    explanation: "Arsenal lost the league again!",
-    emotion: 'Angry',
-    author: tom.username
-  });
+//   var angerId = Emotes.insert({
+//     explanation: "Arsenal lost the league again!",
+//     emotion: 'Angry',
+//     author: tom.username
+//   });
 
-  Comments.insert({
-    emoteId: excitementId,
-    userId: sacha._id,
-    author: sacha.username,
-    submitted: new Date (now - 5 * 3500 * 1000),
-    body: "lol don't jealous me too much now. Keep trying and one day you'll get there."
-  });
+//   Comments.insert({
+//     emoteId: excitementId,
+//     userId: sacha._id,
+//     author: sacha.username,
+//     submitted: new Date (now - 5 * 3500 * 1000),
+//     body: "lol don't jealous me too much now. Keep trying and one day you'll get there."
+//   });
 
-  Comments.insert({
-    emoteId: jealousyId,
-    userId: tom._id,
-    author: tom.username,
-    submitted: new Date (now - 7 * 3500 * 1000),
-    body: "lol don't jealous me too much now. Keep trying and one day you'll get there."
-  });
+//   Comments.insert({
+//     emoteId: jealousyId,
+//     userId: tom._id,
+//     author: tom.username,
+//     submitted: new Date (now - 7 * 3500 * 1000),
+//     body: "lol don't jealous me too much now. Keep trying and one day you'll get there."
+//   });
 
-  Comments.insert({
-    emoteId: angerId,
-    userId: tom._id,
-    author: tom.profile.name,
-    submitted: new Date (now - 12 * 3500 * 1000),
-    body: "lol don't jealous me too much now. Keep trying and one day you'll get there."
-  });
-}
+//   Comments.insert({
+//     emoteId: angerId,
+//     userId: tom._id,
+//     author: tom.profile.name,
+//     submitted: new Date (now - 12 * 3500 * 1000),
+//     body: "lol don't jealous me too much now. Keep trying and one day you'll get there."
+//   });
+// }
 
